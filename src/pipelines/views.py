b2007175aaf10466dde60737ba1c78f165682353
Isac <isac.as0001@gmail.com>
2023-05-01 17:11:04 +0000

    Added pipeline modification

--- src/pipelines/views.py
+++ src/pipelines/views.py
@@ -9,6 +9,8 @@ from src.pipelines.models import Pipeline
 
 pipelines_bp = Blueprint("pipelines", __name__)
 
+debug = True
+
 
 @pipelines_bp.route("/pipelines/get_all", methods=["GET"])
 def get_all_pipelines():
@@ -17,11 +19,6 @@ def get_all_pipelines():
     dictionaries = [pipeline.as_dict() for pipeline in pipelines]
     response = jsonify(dictionaries)
     return response
-
-
-@pipelines_bp.route("/pipelines/read", methods=["POST"])
-def get_pipeline():
-    pass
 
 
 @pipelines_bp.route("/pipelines/create", methods=["POST"])
@@ -41,17 +38,17 @@ def create_pipeline():
         response = jsonify(
             {'result': False, 'message': 'Ya existe un pipeline con ese nombre.', 'pipeline': None})
         return response
-    
+
     new_pipeline = Pipeline(
         name=name,
         description=description,
         strategies=strategies,
         created_by=current_user.username,
         last_modified_by=current_user.username)
-    
+
     db.session.add(new_pipeline)
     db.session.commit()
-    
+
     response = jsonify(
         {'result': True, 'message': '¡Pipeline creado con éxito!', 'pipeline': new_pipeline.as_dict()})
     return response
@@ -59,7 +56,37 @@ def create_pipeline():
 
 @pipelines_bp.route("/pipelines/update", methods=["POST"])
 def update_pipeline():
-    pass
+    # Get the modified pipeline
+    request_pipeline = request.json.get('pipeline')
+
+    try:
+        # Find the pipeline entry in the database
+        pipeline: Pipeline = db.session.execute(
+            db.select(Pipeline).filter_by(id=request_pipeline['id'])).scalar_one()
+    except Exception as e:
+        print(f"[ERROR] - [UPDATE PIPELINE QUERY]:\n{e}\n")
+        response = jsonify(
+            {'result': False, 'message': 'Pipeline not found.', 'pipeline': None})
+        return response
+
+    # Update the database entry
+    pipeline.name = request_pipeline['name']
+    pipeline.description = request_pipeline['description']
+    pipeline.last_modified_by = current_user.username
+    pipeline.updated_at = datetime.now()
+
+    try:
+        # Commiting will update the entry
+        db.session.commit()
+    except Exception as e:
+        print(f"[ERROR] - [UPDATE PIPELINE COMMIT]:\n{e}\n")
+        response = jsonify(
+            {'result': False, 'message': 'Database error, could not commit.', 'pipeline': None})
+        return response
+
+    response = jsonify(
+        {'result': True, 'message': 'Pipeline updated successfully.', 'pipeline': pipeline.as_dict()})
+    return response
 
 
 @pipelines_bp.route("/pipelines/delete", methods=["POST"])
